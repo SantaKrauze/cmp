f0a17c29a80d551fa6234eb50511d69251b37db5
SantaKrauze <gracjantokarz38@gmail.com>
2021-04-28 14:38:30 +0200

    euler and rk4 working fine, ab3 throws out of range

--- cw4/ivp.py
+++ cw4/ivp.py
@@ -1,46 +1,65 @@
 #!/usr/bin/python
 # M. P. Polak & P. Scharoch 2017
 import matplotlib.pyplot as plt
+from math import exp 
 
-def euler(a,b,n,y0,fun):
-    h=(b-a)/n
-    yp=[y0]
-    xp=[a]
-    for i in range(0,n):
+def euler(a, b, n, y0, fun):
+    h = (b-a)/n
+    yp = [y0]
+    xp = [a]
+    for i in range(0, n):
         yp.append(yp[i]+h*fun(xp[i],yp[i]))
         xp.append(xp[i]+h)
     return xp,yp
         
-def rk4(a,b,n,y0,fun):
-    h=(b-a)/n
-    yp=[y0]
-    xp=[a]
-    for i in range(0,n):
-        k1=h*fun(xp[i],yp[i])
-        k2=h*fun(xp[i]+h/2.,yp[i]+k1/2.)
-        k3=h*fun(xp[i]+h/2.,yp[i]+k2/2.)
-        k4=h*fun(xp[i]+h,yp[i]+k3)
+def rk4(a, b, n, y0, fun):
+    h = (b-a)/n
+    yp = [y0]
+    xp = [a]
+    for i in range(0, n):
+        k1 = h*fun(xp[i],yp[i])
+        k2 = h*fun(xp[i]+h/2.,yp[i]+k1/2.)
+        k3 = h*fun(xp[i]+h/2.,yp[i]+k2/2.)
+        k4 = h*fun(xp[i]+h,yp[i]+k3)
         yp.append(yp[i]+(k1+2.*k2+2.*k3+k4)/6.)
         xp.append(xp[i]+h)
     return xp,yp
 
-def ab3(a,b,n,y0,fun):
-    h=(b-a)/n
-    yp=list(y0)
-    xp=[a,a+h,a+h+h]
+def ab3(a, b, n, y0, fun):
+    h = (b-a)/n
+    #yp=list(y0)
+    yp = [y0]
+    xp = [a,a+h,a+h+h]
     for i in range(2,n):
         yp.append(yp[i]+h/12.*(23.*fun(xp[i],yp[i])-16.*fun(xp[i-1],yp[i-1])+5.*fun(xp[i-2],yp[i-2])))
         xp.append(xp[i]+h)
     return xp,yp
 
-def fun(x,y):
+def dFun(x, y):
     return y
 
+def fun(x):
+    return exp(x)
+
 if __name__ == "__main__":
-    plt.plot(euler(0,3,10,0,fun))
-    #plt.plot(rk4(0,3,10,0,fun))
-    #plt.plot(ab3(1,0,1,0,fun))
+    a = 0
+    b = 3
+    n = 10
+    y0 = 1
+    x = []
+    y = []
+    h = (b-a)/n
+    i = 0
+    while i < b:
+        x.append(i)
+        y.append(fun(i))
+        i += h
+
+    eulerX, eulerY = euler(a, b, n, y0, dFun)
+    rkX, rkY = rk4(a, b, n, y0, dFun)
+    #abX, abY = ab3(a, b, n, y0, dFun)
+    plt.plot(x, y)
+    plt.plot(eulerX, eulerY) 
+    plt.plot(rkX, rkY)
+    #plt.plot(abX, abY)
     plt.show()
-    #print(euler(0,3,10,1,fun))
-    #print(rk4(1,0,1000,0,fun))
-    #print(ab3(1,10,1000,0,fun))
